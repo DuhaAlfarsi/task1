fcaff4d19346a885153eab0876ed986bd291001c
balsamnaqa <64330336+balsamnaqa@users.noreply.github.com>
2022-06-26 20:39:19 +0400

    color mode

--- scrip.js
+++ scrip.js
@@ -12,10 +12,51 @@ togglePassword.addEventListener("click", function (e) {
   this.classList.toggle("fa-eye-slash");
 });
 
+//color mode change 
+
+let button1 = document.querySelector("btn1");
+let button3 = document.querySelector("btn3");
+let button2 = document.querySelector("btn2");
+
+button1.addEventListener("click", function(){
+  document.body.style.backgroundColor = "green";
+  button1.removeAttribute('class');
+
+})
+
+button2.addEventListener("click", function(){
+  document.body.style.backgroundColor = "blue";
+  button2.removeAttribute('class');
+
+})
+
+button3.addEventListener("click", function(){
+  document.body.style.backgroundColor = "pink";
+  button2.removeAttribute('class');
+
+})
 
 
 
 
+/* 
+circle1.addEventListener("click", ()=>{
+    document.body.classList.add('green')
+    return
+})
+
+  
+let circle2 = document.querySelector(".color2")
+circle1.addEventListener("click", ()=>{
+    document.body.classList.add('blue')
+    return
+})
+
+   let circle3 = document.querySelector(".color3")   
+   circle1.addEventListener("click", ()=>{
+     document.body.classList.add('pink')
+     return
+})
 
 
-
+ */
